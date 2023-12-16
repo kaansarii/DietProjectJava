df63bc47961ac4e85b459a4b962afbf4d9055c06
UmutcanGuncu <umutcanguncu16@gmail.com>
2023-12-16 23:07:49 +0300

    NutrientsFragment'a deneme listesi eklendi

--- app/src/main/java/com/example/dietapp/NutrientsFragment.java
+++ app/src/main/java/com/example/dietapp/NutrientsFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +17,8 @@ import com.example.dietapp.dtos.GetUserInformationDto;
 import com.example.dietapp.interfaces.IUserInformation;
 
 import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -25,6 +30,8 @@ public class NutrientsFragment extends Fragment {
     TextView fat;
     TextView protein;
     TextView calorie;
+    ListView listBreakfast, listLunch, listDinner;
+    Button listBreakfastItems, listLunchItems, listDinnerItems;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -35,6 +42,13 @@ public class NutrientsFragment extends Fragment {
         fat = rootView.findViewById(R.id.tVFatNutrients);
         protein = rootView.findViewById(R.id.tVProteinNutrients);
         calorie = rootView.findViewById(R.id.tVCalNutrients);
+        listBreakfast = (ListView) rootView.findViewById(R.id.listBreakfast);
+        listBreakfastItems = (Button) rootView.findViewById(R.id.breakfastButton);
+        listLunchItems = (Button) rootView.findViewById(R.id.lunchButton);
+        listLunch = (ListView) rootView.findViewById(R.id.listLunch);
+        listDinnerItems = (Button) rootView.findViewById(R.id.dinnerButton);
+        listDinner = (ListView) rootView.findViewById(R.id.listDinner);
+        List<String> foods = Arrays.asList("Kebap", "Pide", "Lahmacun");
         DecimalFormat decimalFormat = new DecimalFormat("#.##");
         IUserInformation userInformation = RetrofitClient.getRetrofitInstance().create(IUserInformation.class);
         Call<GetUserInformationDto> call = userInformation.getUserInformationWithDailyInformation(8);
@@ -57,7 +71,20 @@ public class NutrientsFragment extends Fragment {
 
             }
         });
-        //return inflater.inflate(R.layout.fragment_nutrients, container, false);
+        listBreakfastItems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, foods);
+                listBreakfast.setAdapter(adapter);
+            }
+        });
+        listLunchItems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, foods);
+                listLunch.setAdapter(adapter);
+            }
+        });
         return rootView;
 
     }
