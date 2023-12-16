750cd856b492bbdefec66a093bbc5e0a79af5c39
UmutcanGuncu <umutcanguncu16@gmail.com>
2023-12-16 23:55:12 +0300

    appUserId ile veri listeleme yeniden eklendi

--- app/src/main/java/com/example/dietapp/NutrientsFragment.java
+++ app/src/main/java/com/example/dietapp/NutrientsFragment.java
@@ -15,7 +15,7 @@ import android.widget.Toast;
 
 import com.example.dietapp.dtos.GetUserInformationDto;
 import com.example.dietapp.interfaces.IUserInformation;
-
+import com.example.dietapp.dtos.SharedId;
 import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.List;
@@ -32,16 +32,17 @@ public class NutrientsFragment extends Fragment {
     TextView calorie;
     ListView listBreakfast, listLunch, listDinner;
     Button listBreakfastItems, listLunchItems, listDinnerItems;
-
+    SharedId sharedId = SharedId.getInstance();
+    int appUserId = sharedId.getSharedData();
     @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_nutrients,container,false);
-        carbonhydrate = rootView.findViewById(R.id.tVCarbNutrients);
-        fat = rootView.findViewById(R.id.tVFatNutrients);
-        protein = rootView.findViewById(R.id.tVProteinNutrients);
-        calorie = rootView.findViewById(R.id.tVCalNutrients);
+        carbonhydrate = rootView.findViewById(R.id.daytVCarbNutrients);
+        fat = rootView.findViewById(R.id.daytVFatNutrients);
+        protein = rootView.findViewById(R.id.daytVProteinNutrients);
+        calorie = rootView.findViewById(R.id.daytVCalNutrients);
         listBreakfast = (ListView) rootView.findViewById(R.id.listBreakfast);
         listBreakfastItems = (Button) rootView.findViewById(R.id.breakfastButton);
         listLunchItems = (Button) rootView.findViewById(R.id.lunchButton);
@@ -51,13 +52,13 @@ public class NutrientsFragment extends Fragment {
         List<String> foods = Arrays.asList("Kebap", "Pide", "Lahmacun");
         DecimalFormat decimalFormat = new DecimalFormat("#.##");
         IUserInformation userInformation = RetrofitClient.getRetrofitInstance().create(IUserInformation.class);
-        Call<GetUserInformationDto> call = userInformation.getUserInformationWithDailyInformation(8);
+        Call<GetUserInformationDto> call = userInformation.getUserInformationWithDailyInformation(appUserId);
         call.enqueue(new Callback<GetUserInformationDto>() {
             @Override
             public void onResponse(Call<GetUserInformationDto> call, Response<GetUserInformationDto> response) {
                 if(response.isSuccessful()){
                     GetUserInformationDto model = response.body();
-                    carbonhydrate.setText("Merhaba");
+                    carbonhydrate.setText(decimalFormat.format(model.getDailyCarbonhydrateRequirement()));
                     fat.setText(decimalFormat.format(model.getDailyFatRequirement()));
                     protein.setText(decimalFormat.format(model.getDailyProteinRequirement()));
                     calorie.setText(decimalFormat.format(model.getDailyCalorieRequirement()));
@@ -83,6 +84,13 @@ public class NutrientsFragment extends Fragment {
             public void onClick(View view) {
                 ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, foods);
                 listLunch.setAdapter(adapter);
+            }
+        });
+        listDinnerItems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, foods);
+                listDinner.setAdapter(adapter);
             }
         });
         return rootView;
