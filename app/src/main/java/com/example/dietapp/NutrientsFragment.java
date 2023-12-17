8f9fd82fb32f8058fdbcff616822f9d3fe8bfe07
KAAN50 <skaan45145@gmail.com>
2023-12-17 21:47:52 +0300

    duzenleme yapildi fragment_nutrients.xml'ee commit

--- app/src/main/java/com/example/dietapp/NutrientsFragment.java
+++ app/src/main/java/com/example/dietapp/NutrientsFragment.java
@@ -3,6 +3,7 @@ package com.example.dietapp;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
